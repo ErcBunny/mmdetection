b0c7c22ad349c0aade2699bc15382ca2772def42
Ryan Y. Liu <yueqianliu@outlook.com>
2023-06-13 02:28:28 +0200

    configs for smaller vitdet models

---
+++ configs/cascade_rcnn/cascade-mask-rcnn_vitmae_b16_sfpn_2_768_1x_coco.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/cascade-mask-rcnn_vitmae_b16_sfpn_2_768.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
