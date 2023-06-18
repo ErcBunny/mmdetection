03b76247e23f65b4232ea1da141b8de3dfb28775
Ryan Y. Liu <yueqianliu@outlook.com>
2023-06-18 18:22:59 +0200

    low lr configs

---
+++ demo/vitdet_small_train_demo_lowlr.py
@@ -0,0 +1,114 @@
+import os
+import mmengine
+import os.path as osp
+import mmcv
+from mmengine import Config
+from mmengine.runner import set_random_seed
+from mmengine.runner import Runner
+
+os.system("wget -c https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip")
+os.system("rm -rf ./ballondatasets/")
+os.system("unzip balloon_dataset.zip -d ./ballondatasets/")
+
+def convert_balloon_to_coco(ann_file, out_file, image_prefix):
+    data_infos = mmengine.load(ann_file)
+
+    annotations = []
+    images = []
+    obj_count = 0
+    for idx, v in enumerate(mmengine.track_iter_progress(data_infos.values())):
+        filename = v['filename']
+        img_path = osp.join(image_prefix, filename)
+        height, width = mmcv.imread(img_path).shape[:2]
+
+        images.append(dict(
+            id=idx,
+            file_name=filename,
+            height=height,
+            width=width))
+
+        bboxes = []
+        labels = []
+        masks = []
+        for _, obj in v['regions'].items():
+            assert not obj['region_attributes']
+            obj = obj['shape_attributes']
+            px = obj['all_points_x']
+            py = obj['all_points_y']
+            poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
+            poly = [p for x in poly for p in x]
+
+            x_min, y_min, x_max, y_max = (
+                min(px), min(py), max(px), max(py))
+
+
+            data_anno = dict(
+                image_id=idx,
+                id=obj_count,
+                category_id=0,
+                bbox=[x_min, y_min, x_max - x_min, y_max - y_min],
+                area=(x_max - x_min) * (y_max - y_min),
+                segmentation=[poly],
+                iscrowd=0)
+            annotations.append(data_anno)
+            obj_count += 1
+
+    coco_format_json = dict(
+        images=images,
+        annotations=annotations,
+        categories=[{'id':0, 'name': 'balloon'}])
+    mmengine.dump(coco_format_json, out_file)
+
+convert_balloon_to_coco(
+    './ballondatasets/balloon/train/via_region_data.json',
+    './ballondatasets/balloon/train/annotation_coco.json',
+    './ballondatasets/balloon/train/')
+convert_balloon_to_coco(
+    './ballondatasets/balloon/val/via_region_data.json',
+    './ballondatasets/balloon/val/annotation_coco.json',
+    './ballondatasets/balloon/val/')
+
+cfg = Config.fromfile('../configs/cascade_rcnn/cascade-mask-rcnn_vitmae_b16_sfpn_6_768_200e_coco_lowlr.py')
+
+# Modify dataset classes and color
+cfg.metainfo = {
+    'classes': ('balloon', ),
+    'palette': [
+        (220, 20, 60),
+    ]
+}
+
+# Modify dataset type and path
+cfg.data_root = './ballondatasets/balloon'
+
+cfg.train_dataloader.dataset.ann_file = 'train/annotation_coco.json'
+cfg.train_dataloader.dataset.data_root = cfg.data_root
+cfg.train_dataloader.dataset.data_prefix.img = 'train/'
+cfg.train_dataloader.dataset.metainfo = cfg.metainfo
+
+cfg.val_dataloader.dataset.ann_file = 'val/annotation_coco.json'
+cfg.val_dataloader.dataset.data_root = cfg.data_root
+cfg.val_dataloader.dataset.data_prefix.img = 'val/'
+cfg.val_dataloader.dataset.metainfo = cfg.metainfo
+
+cfg.test_dataloader = cfg.val_dataloader
+
+# Modify metric config
+cfg.val_evaluator.ann_file = cfg.data_root+'/'+'val/annotation_coco.json'
+cfg.test_evaluator = cfg.val_evaluator
+
+# Modify num classes of the model in box head and mask head
+cfg.model.roi_head.bbox_head[0].num_classes = 1
+cfg.model.roi_head.bbox_head[1].num_classes = 1
+cfg.model.roi_head.bbox_head[2].num_classes = 1
+cfg.model.roi_head.mask_head.num_classes = 1
+cfg.default_hooks.checkpoint.interval = 20
+cfg.optim_wrapper.optimizer.lr = 0.02 / 8
+
+
+# Set up working dir to save files and logs.
+cfg.work_dir = './tutorial_exps/cascade-mask-rcnn_vitmae_b16_sfpn_6_768_200e_coco_lowlr/'
+
+# build the runner from config
+runner = Runner.from_cfg(cfg)
+runner.train()
